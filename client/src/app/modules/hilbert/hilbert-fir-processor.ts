f02d424a6bfe3aa352df241145660b25c15d5f6d
richard-austin <richard.david.austin@gm>
2026-05-15 21:02:14 +0100

    * Handle both channels.

--- client/src/app/modules/hilbert/hilbert-fir-processor.ts
+++ client/src/app/modules/hilbert/hilbert-fir-processor.ts
@@ -1,5 +1,3 @@
-import {designHilbertKernel} from './create-hilbert-fir-filter-node';
-
 export class HilbertFIRProcessor {
   public node!: AudioWorkletNode;
   audioCtx: AudioContext;
@@ -14,50 +12,54 @@ export class HilbertFIRProcessor {
       registerProcessor('hilbert-fir-processor', class Processor extends AudioWorkletProcessor {
         // FIR kernel coefficients (set from parameters)
         kernel: Float32Array;
-        buffer: Float32Array;
-        bufferIndex = 0;
+        buffer: number[][] = [];
+        bufferIndex: number[] = [0,0]
         order: number;
 
         constructor(options: any) {
           super();
           this.kernel = options.processorOptions?.kernel || new Float32Array([1]);
           this.order = this.kernel.length;
-          this.buffer = new Float32Array(this.order);
+          this.buffer[0] = [this.order];
+          this.buffer[1] = [this.order];
           // @ts-ignore
           this.port.onmessage = (event) => {
             if (event.data.type === 'kernel') {
               this.kernel = event.data.kernel;
               this.order = this.kernel.length;
-              this.buffer = new Float32Array(this.order);
+              this.buffer[0] = [this.order];
+              this.buffer[1] = [this.order];
             }
           };
         }
 
-        process(inputs: Float32Array[][], outputs: Float32Array[][]) {
-          const input = inputs[0][0];
-          const output = outputs[0][0];
+        process(inputs: number[][][], outputs: number[][][]) {
+          const output: number[][] = outputs[0];
+          const input: number[][] = inputs[0];
+
           if (!input || !output) return true;
 
-          for (let i = 0; i < input.length; i++) {
-            /* Circular buffer update */
-            this.buffer[this.bufferIndex] = input[i];
+          for (let channel = 0; channel < input.length; ++channel) {
+            const outputChannel: number[] = output[channel];
+            const inputChannel: number[] = input[channel];
+            for (let i = 0; i < inputChannel.length; i++) {
+              /* Circular buffer update */
+              this.buffer[channel][this.bufferIndex[channel]] = inputChannel[i];
 
-            /* FIR convolution */
-            let y = 0;
-            for (let j = 0; j < this.order; j++) {
-              /* Read buffer in reverse (oldest sample first) */
-              const bufferIdx = (this.bufferIndex - j + this.order) % this.order;
-              y += this.buffer[bufferIdx] * this.kernel[j];
+              /* FIR convolution */
+              let y = 0;
+              for (let j = 0; j < this.order; j++) {
+                /* Read buffer in reverse (oldest sample first) */
+                const bufferIdx = (this.bufferIndex[channel] - j + this.order) % this.order;
+                y += this.buffer[channel][bufferIdx] * this.kernel[j];
+              }
+              outputChannel[i] = y;
+              /* Advance buffer pointer */
+              this.bufferIndex[channel] = (this.bufferIndex[channel] + 1) % this.order;
             }
-            output[i] = y;
-
-            /* Advance buffer pointer */
-            this.bufferIndex = (this.bufferIndex + 1) % this.order;
           }
-
           return true;
         }
-
       });
     }
 
