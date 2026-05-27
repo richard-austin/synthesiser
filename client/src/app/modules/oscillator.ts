f3aae7ac2d8c411db9a115e408a3aa475934f150
richard-austin <richard.david.austin@gm>
2026-05-27 21:42:18 +0100

    * SAdd more terms to sawtooth.

--- client/src/app/modules/oscillator.ts
+++ client/src/app/modules/oscillator.ts
@@ -35,7 +35,7 @@ export class Oscillator extends OscFilterBase {
     ),
     new WaveTableDetails(
       "Sawtooth", "sawtooth",
-      {imag: [0, 1, 0.5, 1/3, 1/4, 1/5, 1/6,1/7,1/8,1/9,1/10], real: [0,0,0,0,0,0,0,0,0,0,0]}
+      {imag: [0, 1, 0.5, 1/3, 1/4, 1/5, 1/6,1/7,1/8,1/9,1/10,1/11,1/12,1/13,1/14,1/15,1/16,1/17,1/18,1/19,1/20], real: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
     ),
     new WaveTableDetails(
       "Triangle", "triangle",
