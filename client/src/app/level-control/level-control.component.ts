96cab7151027eb41e5bfd88e226e7ab21cafd3c9
richard-austin <richard.david.austin@gm>
2026-06-11 20:40:16 +0100

    * Change controls to be set by up/down mouse dragging instead of radial.

--- client/src/app/level-control/level-control.component.ts
+++ client/src/app/level-control/level-control.component.ts
@@ -45,26 +45,25 @@ export class LevelControlComponent implements AfterViewInit, OnDestroy {
     this.drawOperationsWorker.postMessage("blur");
   }
 
-  setAngle(currentAngle: number, delta: number): number {
-    currentAngle += delta;
+  setAngle(angle: number): number {
     const p = this.params;
-    const upperLimit = p.plusMinus ? p.calAngle/2 : p.calAngle;
-    const lowerLimit = p.plusMinus ? -p.calAngle/2 :0;
-    if (currentAngle > upperLimit)
-      currentAngle = upperLimit;
-    else if (currentAngle < lowerLimit)
-      currentAngle = lowerLimit;
-    this.setLevel.emit(this.factor() * currentAngle / p.calAngle);
+    const upperLimit = p.plusMinus ? p.calAngle / 2 : p.calAngle;
+    const lowerLimit = p.plusMinus ? -p.calAngle / 2 : 0;
+    if (angle > upperLimit)
+      angle = upperLimit;
+    else if (angle < lowerLimit)
+      angle = lowerLimit;
+    this.setLevel.emit(this.factor() * angle / p.calAngle);
 
-    this.drawOperationsWorker.postMessage({angle: currentAngle});
-    return currentAngle;
+    this.drawOperationsWorker.postMessage({angle: angle});
+    return angle;
   }
 
   currentAngle = 0;
 
   setValue(value: number) {
     let p = this.params;
-    this.currentAngle = this.setAngle(p.calAngle * value/this.factor(), 0);
+    this.currentAngle = this.setAngle(p.calAngle * value / this.factor());
   }
 
   changeStyle(style: dialStyle) {
@@ -76,53 +75,33 @@ export class LevelControlComponent implements AfterViewInit, OnDestroy {
     this.startRender();
     const canvas = this.canvas().nativeElement;
     let mouseDown = false;
-    let lastX = 0
     let lastY = 0;
     canvas.tabIndex = 0;
     canvas.addEventListener('mousedown', (e) => {
-      let p = this.params;
-      let x = e.offsetX - p.centreX;
-      let y = e.offsetY - p.centreY;
+      let y = e.screenY;
       mouseDown = true;
       canvas.focus();
-      if (x >= -this.params.radius && x <= this.params.radius &&
-        y >= -this.params.radius && y <= this.params.radius) {
-        lastX = x;
+      lastY = y;
+      e.preventDefault();
+    });
+
+    window.addEventListener('mousemove', (e) => {
+      if (mouseDown) {
+        let y = e.screenY;
+        let deltaAngle;
+        const delta = lastY - y;
+        deltaAngle = 1.2 *delta;
         lastY = y;
+        this.currentAngle = this.setAngle(this.currentAngle + deltaAngle);
+        e.preventDefault();
       }
-      e.preventDefault();
     });
 
-    canvas.addEventListener('mousemove', (e) => {
-      let p = this.params;
-      let x = e.offsetX - p.centreX;
-      let y = e.offsetY - p.centreY;
-      let deltaAngle = 0;
-      if (mouseDown && x >= -this.params.radius && x <= this.params.radius &&
-        y >= -this.params.radius && y <= this.params.radius) {
-
-        const delta = ((y - lastY) * Math.sign(x) - (x - lastX) * Math.sign(y)) / Math.sqrt(x ** 2 + y ** 2) / 3;
-        deltaAngle = 2.5 * Math.asin(delta) * 180 / Math.PI;
-        lastX = x;
-        lastY = y;
-        if (isNaN(deltaAngle))
-          console.log("deltaAngle = " + deltaAngle);
-        if (!isNaN(deltaAngle) && deltaAngle !== 0)
-          this.currentAngle = this.setAngle(this.currentAngle, deltaAngle);
+    window.addEventListener('mouseup', (e) => {
+      if (mouseDown) {
+        mouseDown = false;
+        e.preventDefault();
       }
-      e.preventDefault();
-    });
-
-    canvas.addEventListener('mouseup', (e) => {
-      lastX = lastY = 0;
-      mouseDown = false;
-      e.preventDefault();
-    });
-
-    canvas.addEventListener('mouseout', (e) => {
-      lastX = lastY = 0;
-      mouseDown = false;
-      e.preventDefault();
     });
 
     canvas.addEventListener('keydown', (e) => {
@@ -131,16 +110,15 @@ export class LevelControlComponent implements AfterViewInit, OnDestroy {
         let p = this.params;
         const key = e.key === "Escape" ? "F0" : e.key;
         const sign = e.shiftKey ? -1 : 1;
-        this.currentAngle = this.setAngle(p.calAngle * sign * parseInt(key.substring(1)) / p.divisions, 0);
-      }
-      else if (e.ctrlKey)
+        this.currentAngle = this.setAngle(p.calAngle * sign * parseInt(key.substring(1)) / p.divisions);
+      } else if (e.ctrlKey)
         delta = 4;
       else if (e.shiftKey)
         delta = 1;
       if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
         if (e.key === "ArrowDown")
           delta *= -1;
-        this.currentAngle = this.setAngle(this.currentAngle, delta);
+        this.currentAngle = this.setAngle(this.currentAngle + delta);
       }
       e.preventDefault();
     });
